5108b82819811e6b54d2efb500a804488fa9aed8
Ad1106 <shuklaadarsh1106@gmail.com>
2021-06-28 15:48:16 +0530

    Initial Commit

---
+++ AdarshShukla/Practical.h
@@ -0,0 +1,39 @@
+void HandleTCPClient(int clntSocket) {
+char buffer[BUFSIZE]; // Buffer for echo string
+
+ // Receive message from client
+ ssize_t numBytesRcvd = recv(clntSocket, buffer, BUFSIZE, 0);
+ if (numBytesRcvd < 0)
+ DieWithSystemMessage("recv() failed");
+ // Send received string and receive again until end of stream
+ while (numBytesRcvd > 0) { // 0 indicates end of stream
+ // Echo message back to client
+ ssize_t numBytesSent = send(clntSocket, buffer, numBytesRcvd, 0);
+ if (numBytesSent < 0)
+ DieWithSystemMessage("send() failed");
+ else if (numBytesSent != numBytesRcvd)
+ DieWithUserMessage("send()", "sent unexpected number of bytes");
+
+ // See if there is more data to receive
+ numBytesRcvd = recv(clntSocket, buffer, BUFSIZE, 0);
+ if (numBytesRcvd < 0)
+ DieWithSystemMessage("recv() failed");
+ }
+
+ close(clntSocket); // Close client socket
+ }
+
+
+
+ void DieWithUserMessage(const char *msg, const char *detail) {
+ fputs(msg, stderr);
+ fputs(": ", stderr);
+ fputs(detail, stderr);
+ fputc('\n', stderr);
+ exit(1);
+ }
+
+ void DieWithSystemMessage(const char *msg) {
+ perror(msg);
+ exit(1);
+ }
